a10a92a42897cdcb98093ce06544dc942addcdcc
Huzaifa <huzaifa@iskaan.com>
2025-12-29 20:42:44 +0500

    Code Update

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
 
-  static const linkdinUrl = "https://www.linkedin.com/in/muhammad-huzaifa-74460720b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app";
+  static const linkdinUrl = "www.linkedin.com/in/huzaifamughal076";
   static const whatsAppPhoneNumber = "03224755176";
   static const email = "huzaifamughal076@gmail.com";
   static const resumeUrl = "https://drive.google.com/file/d/1I2YTwryBP4Udp85jpcwSiC1sFZ00k2Q-/view";
