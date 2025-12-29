8d097f26339c1d27761313a1b1d2d4fa6773843c
Huzaifa <huzaifa@iskaan.com>
2025-12-30 00:28:46 +0500

    Linkedin url update

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
 
-  static const linkdinUrl = "www.linkedin.com/in/huzaifamughal076";
+  static const linkdinUrl = "https://www.linkedin.com/in/huzaifamughal076";
   static const whatsAppPhoneNumber = "03224755176";
   static const email = "huzaifamughal076@gmail.com";
   static const resumeUrl = "https://drive.google.com/file/d/1I2YTwryBP4Udp85jpcwSiC1sFZ00k2Q-/view";
